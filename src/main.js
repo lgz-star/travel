0069e50adf8085fc2b8167a42ab4a223df6eb13b
lgz-star <2537962214@qq.com>
2020-07-20 00:02:04 +0800

    project init

--- src/main.js
+++ src/main.js
@@ -3,8 +3,13 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import fastclick from 'fastclick'           //引用fastclick库，移动端点击方法有300s延迟，使用这个库可以加快相应速度
+import './assets/styles/reset.css'
+import './assets/styles/border.css'
+
 
 Vue.config.productionTip = false
+fastclick.attach(document.body)              //使用fastclick绑定document.body
 
 /* eslint-disable no-new */
 new Vue({
