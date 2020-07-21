ddb6dfacbe8bb7a0575d71b5b0383cd3d6c943aa
lgz-star <2537962214@qq.com>
2020-07-21 16:16:28 +0800

    新增首页轮播图功能

--- README.md
+++ README.md
@@ -19,3 +19,8 @@ npm run build --report
 ```
 
 For a detailed explanation on how things work, check out the [guide](http://vuejs-templates.github.io/webpack/) and [docs for vue-loader](http://vuejs.github.io/vue-loader).
+
+#用到的插件
+轮播图：
+安装：npm install swiper vue-awesome-swiper@2.6.7 --save
+官方：https://github.com/surmon-china/vue-awesome-swiper
---
+++ src/pages/home/components/swiper.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="wrapper">
+    <swiper :options="swiperOptions">
+        <swiper-slide v-for="item of swiperList" :key="item.id">
+          <img class="swiper-img" :src="item.url" />
+        </swiper-slide>
+        <div class="swiper-pagination" slot="pagination"></div>
+    </swiper>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeSwiper',
+  data: function () {
+    return {
+      swiperOptions: {
+        pagination: '.swiper-pagination',
+        loop: 'true'
+      },
+      swiperList: [{
+        id: '001',
+        url: 'https://imgs.qunarzz.com/vs_ceph_vcimg/b8c4527c41649814cc4cf86880abba54.jpeg'
+      }, {
+        id: '002',
+        url: 'https://imgs.qunarzz.com/vc/e3/a1/71/f498dfd3bed948d623c9093252.jpg_92.jpg'
+      }, {
+        id: '003',
+        url: 'https://imgs.qunarzz.com/vc/6d/9f/35/b8ad5468f73fd60ec0ced086f6.jpg_92.jpg'
+      }]
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  .wrapper >>> .swiper-pagination-bullet-active
+    background: #fff
+  //.wrapper设置宽高比为31.25%
+  .wrapper
+    width:100%
+    height: 0
+    overflow: hidden
+    padding-bottom: 31.25%
+    .swiper-img
+      width: 100%
+</style>
