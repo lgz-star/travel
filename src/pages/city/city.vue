d358aa0ad85c2434c43a941ab75d93ff2dc95d03
lgz-star <2537962214@qq.com>
2020-07-27 09:18:23 +0800

    跳转到城市选择页面

---
+++ src/pages/city/city.vue
@@ -0,0 +1,16 @@
+<template>
+  <city-header></city-header>
+</template>
+
+<script>
+import CityHeader from './components/header.vue'
+export default {
+  name: 'city',
+  components: {
+    CityHeader
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+</style>
