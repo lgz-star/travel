978a473911738ea53322920247d0e668b6f346df
lgz-star <2537962214@qq.com>
2020-07-27 09:49:27 +0800

    新增城市页面头部

---
+++ src/pages/city/components/search.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="serch">
+    <input class="serch-input" type="text" placeholder="输入城市名或拼音" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'citysearch'
+}
+</script>
+
+<style lang="stylus" scoped>
+  @import '../../../assets/styles/varible.styl'
+  .serch
+    height: .72rem
+    padding: 0 .1rem
+    background: $bgcolor
+    .serch-input
+      box-sizing: border-box
+      width: 100%
+      height: .62rem
+      padding: 0 .1rem
+      line-height: .62rem
+      text-align: center
+      border-radius: .06rem
+      color:#666
+
+</style>
