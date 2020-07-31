66391bebbfbabce9b085e464ed80379ae41e2f93
lgz-star <2537962214@qq.com>
2020-07-31 13:38:38 +0800

    banner finish

---
+++ src/pages/detail/detail.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <banner></banner>
+  </div>
+</template>
+
+<script>
+import banner from './components/banner.vue'
+export default {
+  name: 'detail',
+  components: {
+    banner
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+</style>
