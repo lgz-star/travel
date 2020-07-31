eb6547b4dd12f6be1c21ff41aee38cc51453b3bd
lgz-star <2537962214@qq.com>
2020-07-31 15:31:18 +0800

    header 渐隐渐现

---
+++ src/pages/detail/components/header.vue
@@ -0,0 +1,90 @@
+<template>
+  <div>
+    <router-link
+      tag="div" to="/"
+      class="header-abs"
+      v-show="showABS"
+    >
+      <div class="iconfont header-abs-back">&#xe624;</div>
+    </router-link>
+    <div
+      class="header-fixed"
+      v-show="!showABS"
+      :style="opacitystyle"
+    >
+      <router-link to="/">
+        <div class="iconfont header-fixed-back">&#xe624;</div>
+      </router-link>
+      景点详情
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'detailheader',
+  data () {
+    return {
+      showABS: true,
+      opacitystyle: {
+        opacity: 0
+      }
+    }
+  },
+  methods: {
+    handlescroll () {
+      const top = document.documentElement.scrollTop
+      if (top > 60) {
+        let opacity = top / 140
+        opacity = opacity > 1 ? 1 : opacity
+        this.opacitystyle = {opacity}
+        this.showABS = false
+      } else {
+        this.showABS = true
+      }
+    }
+  },
+  activated () {
+    // 使用了keep-alive,activated就可以一直使用
+    window.addEventListener('scroll', this.handlescroll)
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  @import '~styles/varible.styl'
+  .header-abs
+    position: absolute
+    left: .2rem
+    top: .2rem
+    width: .8rem
+    height: .8rem
+    border-radius: .4rem
+    line-height: .8rem
+    text-align: center
+    background: rgba(0, 0, 0, .8)
+    .header-abs-back
+      color: #FFF
+      font-size: .4rem
+  .header-fixed
+    position: fixed
+    top: 0
+    left: 0
+    right: 0
+    height: $headerHeight
+    overflow: hidden
+    height1: .86rem
+    line-height: .86rem
+    text-align: center
+    color: #FFFFFF
+    background: $bgcolor
+    font-size: .32rem
+    .header-fixed-back
+      position: absolute
+      top: 0
+      left: 0
+      width: .64rem
+      text-align: center
+      font-size: .4rem
+      color: #fff
+</style>
--- src/pages/detail/detail.vue
+++ src/pages/detail/detail.vue
@@ -1,18 +1,24 @@
 <template>
   <div>
-    <banner></banner>
+    <detail-banner></detail-banner>
+    <detail-header></detail-header>
+    <div class="content"></div>
   </div>
 </template>
 
 <script>
-import banner from './components/banner.vue'
+import DetailBanner from './components/banner.vue'
+import DetailHeader from './components/header.vue'
 export default {
   name: 'detail',
   components: {
-    banner
+    DetailBanner,
+    DetailHeader
   }
 }
 </script>
 
 <style lang="stylus" scoped>
+  .content
+    height: 50rem
 </style>
